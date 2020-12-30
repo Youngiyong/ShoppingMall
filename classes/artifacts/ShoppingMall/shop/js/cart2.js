d5896425f30f65369ebede3987860c7f7092c252
Youngiyong <73825661+Youngiyong@users.noreply.github.com>
2020-12-30 17:14:09 +0900

    맵퍼다시

--- classes/artifacts/ShoppingMall/shop/js/cart2.js
+++ classes/artifacts/ShoppingMall/shop/js/cart2.js
@@ -6,7 +6,9 @@ $(function () {
     console.log(price.length);
     console.log(count.length);
     for (var i = 0; i < price.length; i++) {
-        total_price += Number(price[i])*Number(count[i]);
+        total_price += Number(price.eq(i).text())*Number(count.eq(i).text());
+        console.log(price[i]);
+        console.log(Number(price[i]));
     }
 
     $('#total_price').text(total_price);
